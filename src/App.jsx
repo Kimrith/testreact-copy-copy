c13b6aa3a63ec77844b35bca7c49a81d536baec9
Chey Kimirth <cheykimrith123@gmail>
2025-08-30 21:44:20 +0700

    update

--- src/App.jsx
+++ src/App.jsx
@@ -7,14 +7,29 @@ import Footer from "./Footer";
 
 export default function App() {
   const [products, setProducts] = useState([]);
+  const [isloading, setIsloading] = useState(true);
+
   useEffect(() => {
     const fetchData = async () => {
       const data = await getAllProduct();
       setProducts(data.products);
       console.log(data);
+      setIsloading(false);
     };
     fetchData();
   }, []);
+  if (isloading) {
+    return (
+      <div className="flex justify-center items-center h-screen">
+        <h2 className="text-xl font-bold">
+          <img
+            src="https://i.pinimg.com/originals/fa/6d/c1/fa6dc135745b95f5cb9125669f638349.gif"
+            alt="Loading..."
+          />
+        </h2>
+      </div>
+    );
+  }
   return (
     <div>
       <Navbar
@@ -24,14 +39,17 @@ export default function App() {
         pricing="Pricing"
         contact="Contact"
       />
-      <div className="grid grid-cols-4 gap-4 p-5 ml-15.5 pt-20 flex-wrap">
+      <div className="w-full flex md:flex-wrap p-5  md:justify-center max-sm:justify-start overflow-x-scroll gap-12 pt-20">
         {products.map((product) => (
-          <Card
-            thumbnail={product.thumbnail}
-            price={product.price}
-            title={product.title}
-            rating={product.rating}
-          />
+          <div className="bg-white border border-gray-200 rounded-lg shadow-sm dark:bg-gray-800 dark:border-gray-700 relative w-[300px] flex-shrink-0 max-sm:w-[300px]">
+            <Card
+              key={product.id} // Added key for React's list rendering optimization
+              thumbnail={product.thumbnail}
+              price={product.price}
+              title={product.title}
+              rating={product.rating}
+            />
+          </div>
         ))}
       </div>
       <div className="pt-20">
--- src/Card.jsx
+++ src/Card.jsx
@@ -1,99 +1,46 @@
 import React from "react";
-import Navbar from "./Navbar";
 
-export default function card({ thumbnail, price, title, rating }) {
+export default function Card({ thumbnail, price, title, rating }) {
+  const click = () => {
+    alert(title);
+  };
+
   return (
     <div>
-      <div className="max-w-sm bg-white border border-gray-200 rounded-lg shadow-sm dark:bg-gray-800 dark:border-gray-700 pt-8">
+      <div className="max-w-sm bg-white border border-gray-200 rounded-lg shadow-sm dark:bg-gray-800 dark:border-gray-700">
         <a href="#">
           <img
-            className="rounded-t-lg"
+            className="rounded-t-lg w-100 max-sm:w-[200px] h-[200px] object-cover mx-auto"
             src={
               thumbnail || "https://flowbite.com/docs/images/blog/image-1.jpg"
             }
-            alt=""
+            alt={title || "Product image"}
           />
         </a>
         <div className="p-5">
           <a href="#">
-            <h5 className="mb-2 text-2xl font-bold tracking-tight text-gray-900 dark:text-white">
-              {title || "Undefind"}
+            <h5 className="mb-2 text-2xl font-bold tracking-tight text-gray-900 dark:text-white line-clamp-1">
+              {title || "Undefined"}
             </h5>
           </a>
           <p className="mb-3 font-normal text-gray-700 dark:text-gray-400">
             Price: {price || "Null"}
           </p>
+
+          {/* Rating */}
           <p className="mb-3 font-normal text-gray-700 dark:text-gray-400">
-            <div className="flex items-center">
-              <svg
-                className="w-4 h-4 text-yellow-300 me-1"
-                aria-hidden="true"
-                xmlns="http://www.w3.org/2000/svg"
-                fill="currentColor"
-                viewBox="0 0 22 20"
-              >
-                <path d="M20.924 7.625a1.523 1.523 0 0 0-1.238-1.044l-5.051-.734-2.259-4.577a1.534 1.534 0 0 0-2.752 0L7.365 5.847l-5.051.734A1.535 1.535 0 0 0 1.463 9.2l3.656 3.563-.863 5.031a1.532 1.532 0 0 0 2.226 1.616L11 17.033l4.518 2.375a1.534 1.534 0 0 0 2.226-1.617l-.863-5.03L20.537 9.2a1.523 1.523 0 0 0 .387-1.575Z" />
-              </svg>
-              <svg
-                className="w-4 h-4 text-yellow-300 me-1"
-                aria-hidden="true"
-                xmlns="http://www.w3.org/2000/svg"
-                fill="currentColor"
-                viewBox="0 0 22 20"
-              >
-                <path d="M20.924 7.625a1.523 1.523 0 0 0-1.238-1.044l-5.051-.734-2.259-4.577a1.534 1.534 0 0 0-2.752 0L7.365 5.847l-5.051.734A1.535 1.535 0 0 0 1.463 9.2l3.656 3.563-.863 5.031a1.532 1.532 0 0 0 2.226 1.616L11 17.033l4.518 2.375a1.534 1.534 0 0 0 2.226-1.617l-.863-5.03L20.537 9.2a1.523 1.523 0 0 0 .387-1.575Z" />
-              </svg>
-              <svg
-                className="w-4 h-4 text-yellow-300 me-1"
-                aria-hidden="true"
-                xmlns="http://www.w3.org/2000/svg"
-                fill="currentColor"
-                viewBox="0 0 22 20"
-              >
-                <path d="M20.924 7.625a1.523 1.523 0 0 0-1.238-1.044l-5.051-.734-2.259-4.577a1.534 1.534 0 0 0-2.752 0L7.365 5.847l-5.051.734A1.535 1.535 0 0 0 1.463 9.2l3.656 3.563-.863 5.031a1.532 1.532 0 0 0 2.226 1.616L11 17.033l4.518 2.375a1.534 1.534 0 0 0 2.226-1.617l-.863-5.03L20.537 9.2a1.523 1.523 0 0 0 .387-1.575Z" />
-              </svg>
-              <svg
-                className="w-4 h-4 text-yellow-300 me-1"
-                aria-hidden="true"
-                xmlns="http://www.w3.org/2000/svg"
-                fill="currentColor"
-                viewBox="0 0 22 20"
-              >
-                <path d="M20.924 7.625a1.523 1.523 0 0 0-1.238-1.044l-5.051-.734-2.259-4.577a1.534 1.534 0 0 0-2.752 0L7.365 5.847l-5.051.734A1.535 1.535 0 0 0 1.463 9.2l3.656 3.563-.863 5.031a1.532 1.532 0 0 0 2.226 1.616L11 17.033l4.518 2.375a1.534 1.534 0 0 0 2.226-1.617l-.863-5.03L20.537 9.2a1.523 1.523 0 0 0 .387-1.575Z" />
-              </svg>
-              <svg
-                className="w-4 h-4 text-gray-300 me-1 dark:text-gray-500"
-                aria-hidden="true"
-                xmlns="http://www.w3.org/2000/svg"
-                fill="currentColor"
-                viewBox="0 0 22 20"
-              >
-                <path d="M20.924 7.625a1.523 1.523 0 0 0-1.238-1.044l-5.051-.734-2.259-4.577a1.534 1.534 0 0 0-2.752 0L7.365 5.847l-5.051.734A1.535 1.535 0 0 0 1.463 9.2l3.656 3.563-.863 5.031a1.532 1.532 0 0 0 2.226 1.616L11 17.033l4.518 2.375a1.534 1.534 0 0 0 2.226-1.617l-.863-5.03L20.537 9.2a1.523 1.523 0 0 0 .387-1.575Z" />
-              </svg>
-            </div>
-            : {rating || "Null"}
+            Rating: {rating || "Null"}
           </p>
-          <a
-            href="#"
-            className="inline-flex items-center px-3 py-2 text-sm font-medium text-center text-white bg-blue-700 rounded-lg hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300 dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800"
+
+          {/* Buy Button */}
+          <button
+            className="inline-flex items-center px-3 py-2 text-sm font-medium text-center text-white 
+              bg-blue-700 rounded-lg hover:bg-blue-800 focus:ring-4 focus:outline-none 
+              focus:ring-blue-300 dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800"
+            onClick={() => click()}
           >
             Buy
-            <svg
-              className="rtl:rotate-180 w-3.5 h-3.5 ms-2"
-              aria-hidden="true"
-              xmlns="http://www.w3.org/2000/svg"
-              fill="none"
-              viewBox="0 0 14 10"
-            >
-              <path
-                stroke="currentColor"
-                strokeLinecap="round"
-                strokeLinejoin="round"
-                strokeWidth="2"
-                d="M1 5h12m0 0L9 1m4 4L9 9"
-              />
-            </svg>
-          </a>
+          </button>
         </div>
       </div>
     </div>
